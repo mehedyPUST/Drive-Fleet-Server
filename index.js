f886ef0fd03bcc46d30584328f969643f2096db0
mehedyPUST <mehedy.pust@gmail.com>
2026-05-18 13:09:17 +0600

    car details page api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 const uri = process.env.MONGODB_URI;
-const PORT = process.env.PORT;
-
 const app = express();
+const PORT = process.env.PORT;
+app.use(cors())
+app.use(express.json())
+
 
 
 
@@ -21,12 +24,34 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const db = client.db('Drive-Fleet')
+        const carCollection = db.collection('cars')
+
+        app.get('/cars', async (req, res) => {
+            const result = await carCollection.find().toArray()
+            res.json(result)
+        })
+
+        app.post('/car', async (req, res) => {
+            const carData = req.body
+            const result = carCollection.insertOne(carData)
+            res.json(result)
+        })
+
+
+        app.get('/cars/:id', async (req, res) => {
+            const { id } = req.params
+            const result = await carCollection.findOne({ _id: new ObjectId(id) })
+            res.json(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
